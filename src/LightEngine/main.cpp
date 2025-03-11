032fd041c5e4e02bd9ac9a412f3914499c9a0604
Ryad-MAHIOU <rmahiou@gaming.tech>
2025-03-12 00:21:22 +0100

    add PhysicsScene

--- src/LightEngine/main.cpp
+++ src/LightEngine/main.cpp
@@ -3,7 +3,9 @@
 #include <iostream>
 
 #include "GameManager.h"
+
 #include "SampleScene.h"
+#include "PhysicsScene.h"
 
 #include <cstdlib>
 #include <crtdbg.h>
@@ -12,9 +14,9 @@ int main()
 {
     GameManager* pInstance = GameManager::Get();
 
-	pInstance->CreateWindow(1280, 720, "PhysicsScene", 60, sf::Color::Black);
+	pInstance->CreateWindow(1280, 720, "MiniStudio", 60, sf::Color::Black);
 	
-	pInstance->LaunchScene<SampleScene>();
+	pInstance->LaunchScene<PhysicsScene>();
 
 	return 0;
 }
