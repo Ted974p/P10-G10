96d59ab409c774f5f67e39fd865e2fd2ef7763e0
jauffreyABISSET <jabisset@gaming.tech>
2025-03-17 12:48:17 +0100

    update

--- src/LightEngine/Entities/ButtonEntity.cpp
+++ src/LightEngine/Entities/ButtonEntity.cpp
@@ -30,7 +30,7 @@ void ButtonEntity::onColliding(Entity* other)
 
     if (other->isTag((int)Entity::TAG::Player)) {
         std::cout << "Player detected in collision!" << std::endl;
-        door->goToPosition(300.f, 300.f, 5.f);
+        door->goToPosition(600.f, 300.f, 50.f);
     }
     else {
         std::cout << "This is not the player!" << std::endl;
